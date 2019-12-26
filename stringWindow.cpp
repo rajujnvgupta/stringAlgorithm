43c87951d9c9e5f1fb097e36c9c7ea2980d5674b
rajujnvgupta <39230239+rajujnvgupta@users.noreply.github.com>
2019-12-26 10:55:22 +0530

    first commit

--- stringWindow.cpp
+++ stringWindow.cpp
@@ -1,10 +1,8 @@
-//TODO: program won't work when 1st character is part of minimum string window
-//each character of patter should be at least once into str 
-//code fail for below test case:
-/* 
- * swerty adfgh azxq
- * qas
-*/
+/*
+	@rajujnvgupta
+	26/dec/2019
+//each character of patter should be at least once into window of str
+
 #include<bits/stdc++.h>
 #define int long long int
 using namespace std;
@@ -12,9 +10,7 @@ const int inf = 1e18+2;
 string pat;
 string str;
 string solve(){
-	//if(pat.size() > str.size()){
-		//return "No string";
-	//}
+
 	int pat_size = pat.size();
 	int ans = inf;
 	unordered_map<char, int>mp;
@@ -31,9 +27,6 @@ string solve(){
 	
 	int ptrn_size = ptrn.size();
 	int str_size = str.size();
-	//if(str_size < ptr_size){
-		//return "No string";
-	//}
 	
 	unordered_map<char, char>match;
 	for(int i = 0; i < ptrn_size; i++){
@@ -42,34 +35,23 @@ string solve(){
 	
 	int low = 0;
 	int is_found_all_char_ptrn= 0;
-	//int len = 0;
 	unordered_map<char, int>freq;
 	int window_low = -1, window_hi = -1;
-	int first_char_of_pattern = 0;
-	//int string_size = str.size();
-	
 	//expanding window
-	for(int i = 0; i< str_size; i++){
+	for(int i = 0; i < str_size; i++){
 			if(match.find(str[i]) != match.end()){
-				//len++;
-				first_char_of_pattern = 1;
 				if(freq[str[i]] < 1){
 					is_found_all_char_ptrn++;
 					//low = i;
 				}
 				freq[str[i]]++;
-			}else if(first_char_of_pattern == 0){
-				//if(flag == 0){
-					
-					low++;
-				//}else{
-				//low++;
-					//len++;
-				//}
 			}
+			//else if(first_char_of_pattern == 0){
+					//low++;
+			//}
 			
 			///shringking window
-			if(freq[str[low]] > 1 && is_found_all_char_ptrn == ptrn_size){
+			if(is_found_all_char_ptrn == ptrn_size){ //freq[str[low]] > 1 && 
 				while(freq[str[low]] > 1 || match.find(str[low]) == match.end()){
 					//len--;
 					if(match.find(str[low]) != match.end()){
